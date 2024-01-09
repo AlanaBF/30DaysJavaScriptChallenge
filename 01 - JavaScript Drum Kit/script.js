08d4c20e0722815786214e54868f410ed8bd848a
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-01-09 09:31:17 +0000

    code commented

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,17 +1,25 @@
+// Add an event listener for the 'transitionend' event on the document. This is in the CSS file
 document.addEventListener('transitionend', function(e) {
-    if (e.target.classList.contains('key') && e.propertyName === 'transform') {
-      e.target.classList.remove('playing');
-    }
-  });
-  
-  document.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  
-    if (!audio) return;
-  
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-  });
+  // If the event target has a class of 'key' and the property that transitioned is 'transform'
+  if (e.target.classList.contains('key') && e.propertyName === 'transform') {
+    // Remove the 'playing' class from the target
+    e.target.classList.remove('playing');
+  }
+});
 
+// Add an event listener for the 'keydown' event on the document
+document.addEventListener('keydown', function(e) {
+  // Select the audio element and the key element that correspond to the pressed key
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  // If there's no audio element for this key, end the function
+  if (!audio) return;
+
+  // Set the audio time to 0 (start of the audio clip)
+  audio.currentTime = 0;
+  // Play the audio
+  audio.play();
+  // Add the 'playing' class to the key element
+  key.classList.add('playing');
+});
